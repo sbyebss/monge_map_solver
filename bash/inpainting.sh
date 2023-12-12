fdcc4e93faf38e81a279a604c82ebb2623036305
sbyebss <sbyebss@gmail.com>
2023-12-11 23:23:05 -0500

    fix

--- bash/inpainting.sh
+++ bash/inpainting.sh
@@ -1,3 +1,3 @@
-python run.py name=mse_128_resolution experiment=inpainting_celeb128 model.n_inner_iter=10 mode=paper  model.ema=true logger=wandb
+python run.py name=mse_128_resolution experiment=inpainting_celeb128 model.n_inner_iter=10 mode=paper model.ema=true logger=wandb
 
 python run.py name=mse_64_resolution experiment=inpainting_celeb64 model.n_inner_iter=5 mode=paper model.ema=false logger=wandb
